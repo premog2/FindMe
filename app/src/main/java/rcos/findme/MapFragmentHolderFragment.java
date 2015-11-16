149f53f293ff119a4e9993a87c1a9e0ce6cdd363
lrvolle <lrvolle@gmail.com>
2015-11-16 17:35:50 -0500

    updated dependencies, Added app icon, fixed merge conflicts
    Added app icon

---
+++ app/src/main/java/rcos/findme/MapFragmentHolderFragment.java
@@ -0,0 +1,22 @@
+package rcos.findme;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MapFragmentHolderFragment extends Fragment {
+
+    public MapFragmentHolderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map_fragment_holder, container, false);
+    }
+}
